e2a6688cee88d6646fd18c36b663de89c6adfc69
bob.sun <widekuan@outlook.com>
2017-05-16 17:46:35 +0800

    Test code

--- Example/Holophonor/ViewController.swift
+++ Example/Holophonor/ViewController.swift
@@ -14,16 +14,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let holo = Holophonor.instance
+        holo.addLocalDirectory(dir: NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!)
         holo.rescan {
             print("----RESCANNED----")
             //Debug code
-            let _ = holo.getAllSongs()
-            let _ = holo.getAllAlbums()
-            let _ = holo.getAllArtists()
+//            let _ = holo.getAllSongs()
+//            let _ = holo.getAllAlbums()
+//            let _ = holo.getAllArtists()
             let album = holo.getAlbumBy(name: "Hot Fuss")
             print(album)
             let albums = holo.getAlbumsBy(artist: "The Killers")
             print(albums)
+            
+            let album1 = holo.getAlbumBy(name: "The History Of Rock")
+            for item in (album1?.items)! {
+                print((item as! MediaItem).title)
+                print((item as! MediaItem).fileURL)
+            }
         }
         print("viewDidLoad")
         // Do any additional setup after loading the view, typically from a nib.
