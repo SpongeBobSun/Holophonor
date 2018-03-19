89cae31e6daeaf64e3a84fe2df2dbc8758eaea16
bob.sun <widekuan@outlook.com>
2018-03-19 17:58:38 +0800

    add getArtwork for MediaCollection

--- Holophonor/Classes/Models/MediaCollection+CoreDataClass.swift
+++ Holophonor/Classes/Models/MediaCollection+CoreDataClass.swift
@@ -8,8 +8,22 @@
 
 import Foundation
 import CoreData
+import MediaPlayer
 
 
 public class MediaCollection: NSManagedObject {
-    let _collectionArtwork: UIImage? = nil;
+    var _collectionArtwork: UIImage? = nil;
+    
+    open func getArtworkWithSize(size: CGSize) -> UIImage? {
+        if _collectionArtwork != nil {
+            return _collectionArtwork;
+        }
+        if self.items?.count ?? 0 > 0 {
+            let anyItem = self.items?.anyObject() as! MediaItem
+            _collectionArtwork = anyItem.getArtworkWithSize(size: size)
+            return _collectionArtwork
+        }
+        
+        return nil
+    }
 }
